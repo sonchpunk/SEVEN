cde547f4fa4f5c7655cd588843aee369512dcf0d
sonchpunk <sofa.barbu@bk.ru>
2024-05-23 06:01:51 +0800

    First commit

---
+++ Assets/menu/scripts/Script_Exit.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Script_Exit : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
